c1fe3f0bd82303c446f1a702c4b97af80a97231a
gittemo <jonan.stein@web.de>
2025-05-07 01:10:11 +0800

    triangulated prototype

--- check.js
+++ check.js
@@ -1,10 +1,10 @@
 import fs from 'fs'
 import { info, getPools } from './api.js'
-import { get } from 'http';
+import { liqFmt } from './fmt.js';
 
 const tokens = JSON.parse(fs.readFileSync('tokens.json', 'utf-8'));
 const ctx = await info("spotMetaAndAssetCtxs")
-const prices = {}
+const opps = []
 
 function getL1Price(t1Un, t2Un) {
   const token1Ctx = ctx[1].find(data => data.coin === t1Un);
@@ -17,19 +17,15 @@ async function check(token1, token2, middleToken) {
   const pools1 = await getPools(token1)
   const pools2 = await getPools(middleToken)
   for (const pool of pools1) {
-    const liq = `${Math.floor(pool.liquidity.usd / 1000)}k`;
+    const liq = liqFmt(pool.liquidity.usd);
     if (pool.token2 === token2.name) {
-      prices[`${pool.dexId}/${liq}/${pool.token2}`] = pool.priceNative;
+      opps.push({route: `${pool.dexId}/${liq}/${pool.token2}`, price: pool.priceNative});
     } else if (pool.token2 === middleToken.name) {
       for (const pool2 of pools2) {
-        const liq2 = `${Math.floor(pool2.liquidity.usd / 1000)}k`;
-        if (pool2.token2 === token2.name) {
-          prices[`${pool.dexId}/${liq}/${pool2.dexId}/${liq2}/${pool2.token2}`] = pool.priceNative * pool2.priceNative;
-        } else if (pool2.token2 === token1.name) {
-          prices[`${pool.dexId}/${liq}/${pool2.dexId}/${liq2}/${pool.token2}`] = pool.priceNative / pool2.priceNative;
-        }
+        if (pool2.token2 !== token2.name) continue;
+        const liq2 = liqFmt(pool2.liquidity.usd);
+        opps.push({route: [`${pool.dexId}/${liq}/${pool.token2}`, `${pool2.dexId}/${liq2}/${pool2.token2}`], price: pool.priceNative * pool2.priceNative});
       }
-      prices[`${pool.dexId}/${liq}/${pool.token2}`] = pool.priceNative;
     }
   }
 }
@@ -43,11 +39,10 @@ const middleToken = { name: "WHYPE", address: "0x5555555555555555555555555555555
 //   console.log(pool.token2, pool.baseToken.symbol === middleToken.name ? pool.quoteToken.address : pool.baseToken.address)
 //   console.log(pool.pairAddress)
 // }
-prices.L1 = getL1Price(token1.universeName, token2.universeName);
+opps.push({route: "L1", price: getL1Price(token1.universeName, token2.universeName)})
 
 await check(token1, token2, middleToken);
 console.log(`\n${token1.name}/${token2.name}`);
-for (const [key, value] of Object.entries(prices)) {
-  const price = parseFloat(value).toFixed(10);
-  console.log(`${price} ${key}`);
+for (const opp of opps) {
+  console.log(`${opp.price} ${opp.route}`);
 }
---
+++ fmt.js
@@ -0,0 +1,7 @@
+export function liqFmt(liq) {
+    return `${Math.floor(liq / 1000)}k`
+}
+
+export function routeFmt(liq) {
+    return `${Math.floor(liq / 1000)}k`
+}
