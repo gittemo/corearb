76b061789db9eb8703f97d67428eaefdc98095c4
gittemo <jonan.stein@web.de>
2025-05-07 23:39:00 +0800

    made it easier

--- check.ts
+++ check.ts
@@ -1,20 +1,45 @@
 import fs from 'fs'
 import { info, getPools } from './api.ts'
-import { liqFmt } from './fmt.ts';
+import { liqFmt, round, routeFmt } from './fmt.ts';
 
-const tokens = JSON.parse(fs.readFileSync('tokens.json', 'utf-8'));
+const tokens: { [tokenName: string]: Token } = JSON.parse(fs.readFileSync('tokens.json', 'utf-8'));
 const ctx = await info("spotMetaAndAssetCtxs")
+
+const token1 = tokens.UETH;
+const token2 = tokens.UBTC;
+const middleToken = { name: "WHYPE", address: "0x5555555555555555555555555555555555555555" };
+
+const l1Price = getL1Price(token1.universeName, token2.universeName)
+
+type Token = {
+  name: string;
+  szDecimals: number;
+  weiDecimals: number;
+  index: number;
+  tokenId: string;
+  isCanonical: boolean;
+  evmContract: {
+    address: string;
+    evm_extra_wei_decimals: number;
+  };
+  fullName: string;
+  deployerTradingFeeShare: string;
+  universeName: string;
+  address: string;
+}
 
 type Hop = {
   dex: string;
-  liq: string;
+  liq: number;
+  token1: string;
   token2: string;
-  priceNative: number;
+  price: number;
 }
 
 type Opportunity = {
   route: Hop[];
-  price: number;
+  total: number;
+  change: number
 }
 
 const opps: Opportunity[] = []
@@ -23,39 +48,42 @@ function getL1Price(t1Un, t2Un) {
   const token1Ctx = ctx[1].find(data => data.coin === t1Un);
   const token2Ctx = ctx[1].find(data => data.coin === t2Un);
 
-  return (token1Ctx.markPx / token2Ctx.markPx).toFixed(10);
+  return token1Ctx.markPx / token2Ctx.markPx
 }
 
 async function check(token1, token2, middleToken) {
   const pools1 = await getPools(token1)
   const pools2 = await getPools(middleToken)
   for (const pool of pools1) {
-    const liq = liqFmt(pool.liquidity.usd);
     if (pool.token2 === token2.name) {
-      opps.push({ route: [{ dex: pool.dexId, liq, token2: pool.token2, priceNative: pool.priceNative }], price: pool.priceNative });
+      opps.push({
+        total: pool.priceNative,
+        change: change(pool.priceNative, l1Price),
+        route: [{ dex: pool.dexId, liq: pool.liquidity.usd, token1: token1.name, token2: pool.token2, price: pool.priceNative }],
+      });
     } else if (pool.token2 === middleToken.name) {
       for (const pool2 of pools2) {
         if (pool2.token2 !== token2.name) continue;
-        const liq2 = liqFmt(pool2.liquidity.usd);
-        opps.push({ route: [`${pool.dexId}/${liq}/${pool.token2}`, `${pool2.dexId}/${liq2}/${pool2.token2}`], price: pool.priceNative * pool2.priceNative });
+        opps.push({
+          total: pool.priceNative * pool2.priceNative,
+          change: change(pool.priceNative * pool2.priceNative, l1Price),
+          route: [
+            { dex: pool.dexId, liq: pool.liquidity.usd, token1: token1.name, token2: pool.token2, price: pool.priceNative },
+            { dex: pool2.dexId, liq: pool2.liquidity.usd, token1: middleToken.name, token2: pool2.token2, price: pool2.priceNative },
+          ],
+        });
       }
     }
   }
 }
 
-const token1 = tokens.UETH;
-const token2 = tokens.UBTC;
-const middleToken = { name: "WHYPE", address: "0x5555555555555555555555555555555555555555" };
-// const middleToken = token1;
-// const middleTokenPools = await getPools(middleToken);
-// for (const pool of middleTokenPools) {
-//   console.log(pool.token2, pool.baseToken.symbol === middleToken.name ? pool.quoteToken.address : pool.baseToken.address)
-//   console.log(pool.pairAddress)
-// }
-opps.push({ route: "L1", price: getL1Price(token1.universeName, token2.universeName) })
-
 await check(token1, token2, middleToken);
-console.log(`\n${token1.name}/${token2.name}`);
+console.log(`found ${opps.length} paths`)
 for (const opp of opps) {
-  console.log(`${opp.price} ${opp.route}`);
+  console.log(`\n${routeFmt(opp.route)}`)
+  console.log(`Total: ${round(opp.total, 7)}\tChange: ${round(opp.change, 5)}%`);
+}
+
+function change(a, b) {
+  return ((b - a) / a);
 }
--- fmt.ts
+++ fmt.ts
@@ -2,6 +2,14 @@ export function liqFmt(liq: number) {
   return `${Math.floor(liq / 1000)}k`
 }
 
-export function routeFmt(liq) {
-  return `${Math.floor(liq / 1000)}k`
+export function routeFmt(route) {
+  let str = ""
+  for (const hop of route) {
+    str += `${str ? "" : hop.token1} =${hop.dex}=${liqFmt(hop.liq)}=> ${round(hop.price, 7)} ${hop.token2} `
+  }
+  return str
+}
+
+export function round(num: number, dec = 2) {
+  return Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec)
 }
